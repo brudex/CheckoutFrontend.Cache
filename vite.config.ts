331b066be6fb83cdaac86a10a883160e3f5bcda7
Penrose <brudexgh@gmail.com>
2024-12-26 21:49:50 +0000

    build to output folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: resolve(__dirname, '../checkout.frontend/public'),
+    emptyOutDir: true
+  }
 });
